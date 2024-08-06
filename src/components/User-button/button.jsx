a2c54be7622451168a6e1de1d0ae3fca7a7bcba4
ankurchangani <changaniankur100>
2024-08-06 18:54:01 +0530

    Explorer

--- src/components/User-button/button.jsx
+++ src/components/User-button/button.jsx
@@ -1,20 +1,95 @@
-import '../User-button/button.css'
+// // import '../User-button/button.css'
+
+// // const Button = () => {
+// //     return (
+// //         <div>
+// //             <div className='flex'>
+// //                 <div className='Follow'>
+
+// //                     <button className='Follow'>Follow</button>
+// //                 </div>
+
+// //                 <div className='view-profile'>
+// //                     <button>view profile</button>
+// //                 </div>
+// //             </div>
+// //         </div>
+// //     )
+// // }
+
+// // export default Button
+
+// import React, { useState } from 'react';
+// import '../User-button/button.css';
+
+// const Button = () => {
+   
+//     const [isFollowing, setIsFollowing] = useState(false);
+
+    
+//     const ClickFollowing = () => {
+//         setIsFollowing(!isFollowing);
+//     };
+
+//     return (
+//         <div>
+//             <div className='flex'>
+//                 <div className='Follow'>
+//                     <button
+//                         className={`Follow ${isFollowing ? 'following' : ''}`}
+//                         onClick={ClickFollowing}
+//                     >
+//                         {isFollowing ? 'Following' : 'Follow'}
+//                     </button>
+//                 </div>
+
+//                 <div className='view-profile'>
+//                     <button>View Profile</button>
+//                 </div>
+//             </div>
+//         </div>
+//     );
+// };
+
+// export default Button;
+
+
+import React, { useState } from 'react';
+import '../User-button/button.css';
 
 const Button = () => {
+    const [isFollowing, setIsFollowing] = useState(false);
+    const [message, setMessage] = useState('');
+
+    const ClickFollowing = () => {
+        setIsFollowing(!isFollowing);
+    };
+
+    const ViewProfile = () => {
+        setMessage('Profile view successful!');
+       
+        setTimeout(() => setMessage(''), 3000); 
+    };
+
     return (
         <div>
             <div className='flex'>
                 <div className='Follow'>
-
-                    <button className='Follow'>Follow</button>
+                    <button
+                        className={`Follow ${isFollowing ? 'following' : ''}`}
+                        onClick={ClickFollowing}
+                    >
+                        {isFollowing ? 'Following' : 'Follow'}
+                    </button>
                 </div>
 
                 <div className='view-profile'>
-                    <button>view profile</button>
+                    <button onClick={ViewProfile}>View Profile</button>
                 </div>
             </div>
+            {message && <p className='message'>{message}</p>} 
         </div>
-    )
-}
+    );
+};
 
-export default Button
+export default Button;
